36b8aa744674baf387b52c164510149c5004e818
rather stay enigma <ranvir.matrix@gmail.com>
2026-02-28 06:02:12 +0530

    multer adjusted

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const { createClient } = require('@supabase/supabase-js');
-
+const multer = require('multer');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
